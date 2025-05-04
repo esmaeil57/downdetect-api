30e6ff2c6bf6b0ef0310c3113e280b20aa2b90b3
esmaeil_a57 <esmaeilahmed51@gmail.com>
2025-05-04 12:34:18 +0300

    edit port

--- src/index.js
+++ src/index.js
@@ -34,5 +34,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on : http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
